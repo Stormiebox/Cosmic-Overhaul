21162862b37828bc778f568494daedcf04a8568d
HazyLunar <erikf08@gmail.com>
2026-06-06 22:05:36 -0700

    Further updates.

--- data/scripts/galaxy/init.lua
+++ data/scripts/galaxy/init.lua
@@ -1,12 +1,4 @@
-local co_old_galaxy_init = initialize
-function initialize(...)
-    if co_old_galaxy_init then co_old_galaxy_init(...) end
-
-    if onServer() then
-        local galaxy = Galaxy()
-        if galaxy then
-            galaxy:addScriptOnce("data/scripts/galaxy/factoryregister.lua")
-            galaxy:addScriptOnce("data/scripts/galaxy/galaxymapqol.lua")
-        end
-    end
+if onServer() then
+    Galaxy():addScriptOnce("data/scripts/galaxy/factoryregister.lua")
+    Galaxy():addScriptOnce("data/scripts/galaxy/galaxymapqol.lua")
 end
